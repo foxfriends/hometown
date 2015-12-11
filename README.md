854045efa6ce18b5eee72c92673138670c3e7fcd
Cameron Eldridge <cameron_e@rogers.com>
2015-12-11 13:19:28 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Hometown
+Build a virtual town. Have your friends over, take care of your villagers, and
+make everyone happy.
+
+Inspired by Animal Crossing.
---
+++ test/test.js
@@ -0,0 +1,5 @@
+'use strict';
+let expect = require('chai').expect;
+describe('hometown', function() {
+    it('should exist', () => expect(3).to.equal(3));
+});
